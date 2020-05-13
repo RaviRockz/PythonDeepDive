e2481f7cca4758ab19b15a8ca2777707fe969e68
RaviRockz <ravirockz007.rk@gmail.com>
2020-05-13 18:02:45 +0530

    Initial commit 1

---
+++ 1_Functional/6_ScopesClosuresDecorators/7_memoization.py
@@ -0,0 +1,27 @@
+def memoize(fn):
+    cache = {}
+
+    def inner(n):
+        if n not in cache:
+            cache[n] = fn(n)
+        return cache[n]
+    return inner
+
+
+@memoize
+def fib(n):
+    print(f'Calculating fib({n})')
+    return 1 if n < 3 else fib(n-1) + fib(n-2)
+
+
+@memoize
+def fact(n):
+    print(f'Calculating {n}!')
+    return 1 if n < 2 else n * fact(n-1)
+
+
+print(fib(10))
+print(fib(11))
+print(fact(6))
+print(fact(10))
+print(fact(6))
